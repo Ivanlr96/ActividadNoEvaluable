1d281045fdfe358a82ce5251881ee84239aa6cc7
unknown <ivanlorenzoruiz@alumnos.tuniversformacion.es>
2023-10-03 10:51:44 +0200

    Añadidas la funcionalidad

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+
+function insertar () {
+let numero;
+numero=prompt ('Inserte un número');
+if (parseInt.numero>0) {
+    alert("El número ingresado es positivo")
+    console.log("El número ingresado es positivo")
+}
+else if (parseInt.numero <0) {
+alert("El numero es negativo")
+console.log("El número ingresado es negativo")
+}
+else if (parseInt.numero=0) {
+alert ("El número es igual a 0")
+console.log("El número ingresado es igual a 0")
+}
+else {
+    alert ("Inserte un número correcto")
+
+}
+}
+
+
+
